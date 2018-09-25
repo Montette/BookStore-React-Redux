6a612e81e1bb61c464d0ca20b3613a154685e809
Monika Radosiewicz <monika.radosiewicz@gmail.com>
2018-09-25 19:24:50 +0200

    add child compoments, refactor tests for App

---
+++ src/components/Order/Order.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const Order = (props) => {
+    
+    return (
+        <div>
+       
+        
+        </div>
+    );
+}
+
+export default Order;
---
+++ src/components/Order/Order.test.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Order from './Order';
+
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure ({adapter: new Adapter()})
+
+describe('order tests', ()=> {
+
+
+    it('order renders without crashing', ()=> {
+       shallow(<Order />)
+       
+    })
+
+})
