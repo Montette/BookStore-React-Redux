56ac99e887a1b8819db3edaeb2021025439eb6b2
Monika Radosiewicz <monika.radosiewicz@gmail.com>
2018-09-25 20:58:58 +0200

    add form to AdminPanel

--- src/components/AdminPanel/AdminPanel.js
+++ src/components/AdminPanel/AdminPanel.js
@@ -1,14 +1,33 @@
 import React from 'react';
+import { Form, Input, Label, TextArea } from './style'
 
-
-const AdminPanel = (props) => {
-    
-    return (
-        <div>
+class AdminPanel extends React.Component {
+    render (){
+        return (
+            <div>
+                <Form>
+                    
+                    <Label for='bookNameInput'>Book name:
+                    <Input type='text' placeholder='Book Name' id='bookNameInput' name='bookName'/>
+                    </Label>
+                    <Label for='bookAuthorInput'>Book author:
+                    <Input type='text' placeholder='Book Author' id='bookAuthorInput' name='bookAuthor'/>
+                    </Label>
+                    <Label for='bookAuthorInput'>Book description:
+                    <TextArea  placeholder='Book Description' id='bookDescriptionInput' name='bookDescription'/>
+                    </Label>
+                    <Label for='bookAuthorInput'>Book Image:
+                    <Input type='file' id='bookImageInput' name='bookImage'/>
+                    </Label>
+                    <Label for='bookAuthorInput'>On stock:
+                    <input type='checkbox'  placeholder='bookOnStock' id='bookOnStock' name='bookOnStock'/>
+                    </Label>
+                    <button type='submite'>Save</button>
+                </Form>
             
-        
-        </div>
-    );
+            </div>
+        );
+    }
 }
 
 export default AdminPanel;
---
+++ src/components/AdminPanel/style.js
@@ -0,0 +1,39 @@
+import styled from 'styled-components';
+
+export const Form = styled.form`
+font-size: 13px;
+padding: 8px 10px;
+display: flex;
+max-width: 400px;
+width: 100%;
+flex-wrap: wrap;
+text-align: left;
+
+`
+
+export const Input = styled.input`
+display: block;
+padding-left: 5px;
+color: darkGrey;
+background-color: #fdfcfc;
+margin-top: 5px;
+height: 30px;
+border-radius: 3px;
+width: 100%;
+`
+export const Label = styled.label`
+        display: block;
+        margin-bottom: 20px;
+        margin-top: 7px;
+        font-weight: bold;
+        color: darkGrey;
+        width: 100%;
+`
+export const TextArea = styled.textarea`
+display: block;
+margin-bottom: 20px;
+margin-top: 7px;
+font-weight: bold;
+color: darkGrey;
+width: 100%;
+`
